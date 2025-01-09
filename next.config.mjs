cc524a6eefe9ad81243264778fe552ce8be5e302
Lucas Hang <lucas.hhang@gmail.com>
2025-01-09 14:13:00 -0300

    ci: allow any subdomain for graphassets

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ const nextConfig = {
     images: {
         remotePatterns: [
             { protocol: 'https', hostname: 'media.graphcms.com' },
-            { protocol: 'https', hostname: 'media.graphassets.com' },
+            { protocol: 'https', hostname: '**.graphassets.com' },
             { protocol: 'https', hostname: 'www.youtube.com' },
             { protocol: 'https', hostname: 'img.youtube.com' },
             { protocol: 'https', hostname: 'player.vimeo.com' },
